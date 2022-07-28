9261372dbe9dab9b52fd3970f9da7c68b3142c42
alkhous <alkhousahmad@gmail.com>
2022-07-28 14:51:27 +0200

    add pagination component

---
+++ src/components/molecules/pagination/Pagination.tsx
@@ -0,0 +1,37 @@
+import ReactPaginate, { ReactPaginateProps } from "react-paginate";
+import Text from "../../atoms/text";
+
+export interface PaginationProps extends ReactPaginateProps {
+  totalEntries: number;
+  pageSize: number;
+}
+
+const Pagination: React.FC<PaginationProps> = (props) => {
+  return (
+    <div className="flex flex-col w-full  items-center">
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel=">"
+        onPageChange={props.onPageChange}
+        pageRangeDisplayed={props.pageRangeDisplayed}
+        pageCount={props.pageCount}
+        previousLabel="<"
+        containerClassName={
+          "flex relative inline-flex items-center px-2 py-2 rounded border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50 " +
+          props.containerClassName
+        }
+        previousClassName="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+        nextClassName="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+        pageClassName="grow flex mx-2"
+        pageLinkClassName="grow bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium justify-center"
+        activeLinkClassName="z-10 bg-indigo-50 border-indigo-500 text-indigo-600 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
+      />
+
+      <Text tag="span" className="text-xl py-2">
+        showing {props.pageSize} of total {props.totalEntries}
+      </Text>
+    </div>
+  );
+};
+
+export default Pagination;
