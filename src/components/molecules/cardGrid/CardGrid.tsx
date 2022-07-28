ad0a3ec71625a9478dc4a635e8c751ad21528630
alkhous <alkhousahmad@gmail.com>
2022-07-28 15:55:27 +0200

    Add Card Grid Component

---
+++ src/components/molecules/cardGrid/CardGrid.tsx
@@ -0,0 +1,40 @@
+import { Absence } from "../../../common/types/api/absence";
+import Text from "../../atoms/text";
+import AbsenceCard from "../absenceCard";
+import PlaceholderCard from "../placeholderCard";
+
+export interface CardGridProps {
+  data?: Absence[];
+  isLoading: boolean;
+  error?: string;
+}
+
+const CardGrid: React.FC<CardGridProps> = ({ data, isLoading, error }) => {
+  let content = <></>;
+
+  if (error) {
+    content = (
+      <div className="h-full bg-white-500  bg-obacit-50 z-50 flex justify-center items-center">
+        <Text tag="p" className="text-red-600 text-2xl">
+          {error}
+        </Text>
+      </div>
+    );
+  } else {
+    if (isLoading) {
+      let cards = data?.map((element: Absence) => {
+        return <PlaceholderCard />;
+      });
+      content = <div className="grid grid-cols-5  gap-3">{cards}</div>;
+    } else {
+      let cards = data?.map((element: Absence) => {
+        return <AbsenceCard {...element} />;
+      });
+      content = <div className="grid grid-cols-5  gap-3">{cards}</div>;
+    }
+  }
+
+  return <>{content}</>;
+};
+
+export default CardGrid;
