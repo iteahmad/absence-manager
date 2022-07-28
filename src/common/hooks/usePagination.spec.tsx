db4c0eb385126dacf402e9339e3a87cb6c01b157
alkhous <alkhousahmad@gmail.com>
2022-07-28 15:31:26 +0200

    usepagination test : remove unused imports

--- src/common/hooks/usePagination.spec.tsx
+++ src/common/hooks/usePagination.spec.tsx
@@ -1,7 +1,6 @@
-import { renderHook, RenderHookResult } from "@testing-library/react";
+import { RenderHookResult } from "@testing-library/react";
 import { act } from "react-dom/test-utils";
-import { Provider } from "react-redux";
-import { RootState, setupStore } from "../../store";
+
 import {
   absencePagination,
   absenceSelector,
