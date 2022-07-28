c309cff6bf9752c9c5bf32c3cb8fea85924a4351
alkhous <alkhousahmad@gmail.com>
2022-07-28 22:04:57 +0200

    enhance design

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,7 +5,7 @@ import AbsencePanel from "../components/organisms/AbsencePanel";
 const HomePage = () => {
   return (
     //<div className="h-screen  flex flex-col p-5 items-stretch  justify-center ">
-    <div className="h-full container flex flex-col p-5 items-stretch  justify-center ">
+    <div className="h-full mx-auto container flex flex-col p-5 items-stretch  justify-center ">
       <div className="basis-1/12 w-full  rounded flex items-center justify-center">
         <Heading> Absence Manager</Heading>
       </div>
